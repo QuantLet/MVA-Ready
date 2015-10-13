0119e808939b8afa03adf1c7f8bb0dc1eda66c5e
Svetlana Bykovskaya <bykovskaya.svetlana@gmail.com>
2015-10-13 17:48:03 +0300

    Update MVAMDScity1.r

--- QID-930-MVAMDScity1/MVAMDScity1.r
+++ QID-930-MVAMDScity1/MVAMDScity1.r
@@ -3,18 +3,21 @@
 # −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
 # Published in : Applied Multivariate Statistical Analysis
 # −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
-# Description : Computes the map of the cities by application 
-# of multidimensional scaling.
+# Description : Computes the map of the cities by 
+# application of multidimensional scaling.
 # −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
-# Keywords : MDS, multi-dimensional, scaling, plot, graphical representation
+# Keywords : MDS, multi-dimensional, scaling, plot, 
+# graphical representation
 # −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
-# See also : MVAMDScity2, MVAMDSnonmstart, MVAMDSpooladj, MVAmdscarm, MVAnmdscar1, MVAnmdscar2, MVAnmdscar3
+# See also : MVAMDScity2, MVAMDSnonmstart, MVAMDSpooladj, 
+# MVAmdscarm, MVAnmdscar1, MVAnmdscar2, MVAnmdscar3
 # −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
 # Author : Zografia Anastasiadou
 # −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
 # Submitted : Tue, October 28 2014 by Felix Jung
 # −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
-# Example : Metric MDS solution for the intercity road distances.
+# Example : Metric MDS solution for the intercity road 
+# distances.
 # −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
 
 # clear all variables
@@ -22,25 +25,25 @@ rm(list = ls(all = TRUE))
 graphics.off()
 
 # Intercity road distances
-ber = c(0, 214, 279, 610, 596, 237)
-dre = c(214, 0, 492, 533, 496, 444)
-ham = c(279, 492, 0, 520, 772, 140)
-kob = c(610, 533, 520, 0, 521, 687)
-mue = c(596, 496, 772, 521, 0, 771)
-ros = c(237, 444, 140, 687, 771, 0)
+ber  = c(0, 214, 279, 610, 596, 237)
+dre  = c(214, 0, 492, 533, 496, 444)
+ham  = c(279, 492, 0, 520, 772, 140)
+kob  = c(610, 533, 520, 0, 521, 687)
+mue  = c(596, 496, 772, 521, 0, 771)
+ros  = c(237, 444, 140, 687, 771, 0)
 
 dist = cbind(ber, dre, ham, kob, mue, ros)
 
-# a,b,h,i are matrices
-a = (dist^2) * (-0.5)
-i = diag(6)
-u = rep(1, 6)
-h = i - (1/6 * (u %*% t(u)))
-b = h %*% a %*% h                       # Determine the inner product matrix
-e = eigen(b)
-x = e$vectors %*% (diag(e$values)^0.5)  # Determine the coordinate matrix
+# a, b, h, i are matrices
+a    = (dist^2) * (-0.5)
+i    = diag(6)
+u    = rep(1, 6)
+h    = i - (1/6 * (u %*% t(u)))
+b    = h %*% a %*% h                       # Determine the inner product matrix
+e    = eigen(b)
+x    = e$vectors %*% (diag(e$values)^0.5)  # Determine the coordinate matrix
 
-# Plot
+# Plot: Initial Configuration
 plot(x[, 1], x[, 2], xlim = c(-500, 700), ylim = c(-500, 300), xlab = "NORTH - SOUTH - DIRECTION in km", 
     ylab = "EAST - WEST - DIRECTION in km", main = "Initial Configuration", cex.axis = 1.2, 
     cex.lab = 1.2, cex.main = 1.8)
