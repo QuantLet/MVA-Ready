164e03fe8d8850ad6caba1235833ef21d1bf0edd
Svetlana Bykovskaya <bykovskaya.svetlana@gmail.com>
2015-10-17 14:54:36 +0300

    Update MVAportfol.r

--- QID-1226-MVAportfol/MVAportfol.r
+++ QID-1226-MVAportfol/MVAportfol.r
@@ -26,85 +26,85 @@ rm(list = ls(all = TRUE))
 graphics.off()
 
 # load data
-ibm 	= read.csv("ibm.csv")
-apple 	= read.csv("apple.csv")
-bac 	= read.csv("bac.csv")
-ford 	= read.csv("ford.csv")
-ed 	= read.csv("ed.csv")
-ms 	= read.csv("ms.csv")
+ibm   = read.csv("ibm.csv")
+apple = read.csv("apple.csv")
+bac   = read.csv("bac.csv")
+ford  = read.csv("ford.csv")
+ed    = read.csv("ed.csv")
+ms    = read.csv("ms.csv")
 
 # compute returns for IBM
-y1 	= ibm[, 5]
-a 	= 0
-i 	= 1
+y1 = ibm[, 5]
+a  = 0
+i  = 1
 while (i <= 120) {
-    i 		= i + 1
-    a[i] 	= (y1[i] - y1[i - 1])/y1[i]
+    i    = i + 1
+    a[i] = (y1[i] - y1[i - 1])/y1[i]
 }
 x1 = a[2:121] 	
 
 # compute returns for Apple
-y2 	= apple[, 5]
-b 	= 0
-i 	= 1
+y2 = apple[, 5]
+b  = 0
+i  = 1
 while (i <= 120) {
-    i		= i + 1
-    b[i]	= (y2[i] - y2[i - 1])/y2[i]
+    i    = i + 1
+    b[i] = (y2[i] - y2[i - 1])/y2[i]
 }
-x2 	= b[2:121] 	
+x2 = b[2:121] 	
 
 # compute returns for Bank of America Corporation
-y3 	= bac[, 5]
-d 	= 0
-i 	= 1
+y3 = bac[, 5]
+d  = 0
+i  = 1
 while (i <= 120) {
-    i 		= i + 1
-    d[i] 	= (y3[i] - y3[i - 1])/y3[i]
+    i    = i + 1
+    d[i] = (y3[i] - y3[i - 1])/y3[i]
 }
-x3 	= d[2:121]
+x3 = d[2:121]
 
 # compute returns for Forward Industries
-y4 	= ford[, 5]
-f 	= 0
-i 	= 1
+y4 = ford[, 5]
+f  = 0
+i  = 1
 while (i <= 120) {
-    i 		= i + 1
-    f[i] 	= (y4[i] - y4[i - 1])/y4[i]
+    i    = i + 1
+    f[i] = (y4[i] - y4[i - 1])/y4[i]
 }
-x4 	= f[2:121]
+x4 = f[2:121]
 
 # compute returns for Consolidated Edison
-y5 	= ed[, 5]
-g 	= 0
-i 	= 1
+y5 = ed[, 5]
+g  = 0
+i  = 1
 while (i <= 120) {
-    i 		= i + 1
-    g[i] 	= (y5[i] - y5[i - 1])/y5[i]
+    i    = i + 1
+    g[i] = (y5[i] - y5[i - 1])/y5[i]
 }
-x5 	= g[2:121]
+x5 = g[2:121]
 
 # compute returns for Morgan Stanley
-y6 	= ms[, 5]
-h 	= 0
-i 	= 1
+y6 = ms[, 5]
+h  = 0
+i  = 1
 while (i <= 120) {
-    i 		= i + 1
-    h[i] 	= (y6[i] - y6[i - 1])/y6[i]
+    i    = i + 1
+    h[i] = (y6[i] - y6[i - 1])/y6[i]
 }
-x6 	= h[2:121]
+x6 = h[2:121]
 
-ix 	= c(1:6) 							# choose assets to use
-x 	= cbind(x1, x2, x3, x4, x5, x6) 				# MVAportfolio of all assets
-s1 	= solve(cov(x)) 						# inverse of empirical variance
-one 	= rep(1, length(ix)) 						# vector of ones
-c2 	= (s1 %*% one)/rep(t(one) %*% s1 %*% one, length(s1 %*% one)) 	# c2 weight
-c1 	= one/rep(sum(one), length(one)) 				# c1 weight
-q1 	= x %*% c1  							# Optimal MVAportfol returns 
-q2 	= x %*% c2  							# Nonoptimal MVAportfol returns 
+ix  = c(1:6)                                # choose assets to use
+x   = cbind(x1, x2, x3, x4, x5, x6)         # MVAportfolio of all assets
+s1  = solve(cov(x))                         # inverse of empirical variance
+one = rep(1, length(ix))                    # vector of ones
+c2  = (s1 %*% one)/rep(t(one) %*% s1 %*% one, length(s1 %*% one))    # c2 weight
+c1  = one/rep(sum(one), length(one))        # c1 weight
+q1  = x %*% c1                              # Optimal MVAportfol returns 
+q2  = x %*% c2                              # Nonoptimal MVAportfol returns 
 
-t 	= c(1:120)
-d1 	= cbind(t, q1)
-d2 	= cbind(t, q2)
+t  = c(1:120)
+d1 = cbind(t, q1)
+d2 = cbind(t, q2)
 
 # plot
 par(mfrow = c(2, 1))
