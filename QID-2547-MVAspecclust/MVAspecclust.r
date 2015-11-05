b285849aed112c3d9c0568a19fc79d46c48771cb
lborke <lukas.borke@web.de>
2015-11-05 13:27:29 +0100

    add q 2547

---
+++ QID-2547-MVAspecclust/MVAspecclust.r
@@ -0,0 +1,51 @@
+# −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
+# Name of QuantLet : MVAspecclust
+# −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
+# Published in : Applied Multivariate Statistical Analysis
+# −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
+# Description : MVAspecclust computes the clusters for the exemplary data 
+# based on the Euclidean distance and predefined number of clusters.
+# −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
+# Keywords : cluster-analysis, spectral, plot, graphical representation, 
+# distance, euclidean, data visualization
+# −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
+# See also : MVAclus8p, MVAclusbank, MVAclusbh, MVAclusfood, 
+# MVAclususcrime, SMSclus8p, SMSclus8pd, SMSclus8pmst2, 
+# SMSclusbank, SMScluscomp, SMScluscrime, SMScluscrimechi2, 
+# SMSclushealth
+# −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
+# Author : Piotr Majer
+# −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
+# Submitted : Sun, September 07 2014 by Awdesch Melzer
+# −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
+# Datafile : data_example.dat
+# −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
+# Example 1 : Plot of raw data.
+# −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
+# Example 2 : Plot of derived clusters.
+# −−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
+
+# clear all variables
+rm(list = ls(all = TRUE))
+graphics.off()
+
+# install and load packages
+libraries = c("kernlab")
+lapply(libraries, function(x) if (!(x %in% installed.packages())) {
+    install.packages(x)
+})
+lapply(libraries, library, quietly = TRUE, character.only = TRUE)
+
+# load data
+data = t(read.table("data_example.dat", quote = ""))
+sc = specc(data, centers = 4)
+centers(sc)
+size(sc)
+withinss(sc)
+
+# plot 1: raw data
+plot(data, col = "black", main = "Raw Data", xlab = "", ylab = "", cex.axis = 2, lwd = 2, pch = 16)
+
+# plot 2: derived clusters
+dev.new("")
+plot(data, col = sc, main = "Derived Clusters", xlab = "", ylab = "", cex.axis = 2, lwd = 2, pch = 16) 
---
+++ QID-2547-MVAspecclust/README.md
@@ -0,0 +1,2 @@
+# QID-2547-MVAspecclust
+computes the clusters for the exemplary data data_example.dat based on the Euclidean distance and predefined number of clusters It is also used to derive clusters for fMRI data (provided upon request at SFB649)
